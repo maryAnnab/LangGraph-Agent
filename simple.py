0063852763529992375b5f89db12cdf8f50809f4
maryAnnab <bielechanna@gmail.com>
2025-10-04 18:49:32 +0200

    update

--- simple.py
+++ simple.py
@@ -1,8 +1,6 @@
 from typing import Annotated
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
-from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -28,8 +26,7 @@ def chatbot(state: State):
 # The second argument is the function or object that will be called whenever
 # the node is used.
 graph_builder.add_node("chatbot", chatbot)
-graph_builder.add_edge(START, "chatbot")
-graph_builder.add_edge("chatbot", END)
+
 
 
 graph = graph_builder.compile()
